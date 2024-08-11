9592c2d776275ba02ec958460945193505b0d996
gustigiyus <gustiggt123@gmail.com>
2024-08-11 11:09:00 +0700

    Add news and Gopaylater

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_latihan_slicing_2/datas/icons.dart';
+import 'package:flutter_latihan_slicing_2/datas/news.dart';
 import 'package:flutter_latihan_slicing_2/theme.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -80,6 +81,7 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // Search Bar and Profile Pictures
           Padding(
@@ -428,6 +430,177 @@ class Body extends StatelessWidget {
           ),
 
           // Instant Access
+          Padding(
+            padding: const EdgeInsets.only(left: 15, right: 15, top: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Akses Cepat',
+                  style: bold18.copyWith(color: dark1),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(
+                      color: const Color(0xFFE8E8E8),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      ...[
+                        'Pintu masuk motor, MNC Land',
+                        'Pintu keluar motor, MNC Land'
+                      ].map(
+                        (text) => Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 24,
+                                height: 24,
+                                decoration: BoxDecoration(
+                                  color: green2,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: SvgPicture.asset(
+                                  'assets/icons/goride.svg',
+                                  // ignore: deprecated_member_use
+                                  color: Colors.white,
+                                  width: 24,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 12,
+                              ),
+                              Flexible(
+                                fit: FlexFit.tight,
+                                child: Text(
+                                  text,
+                                  style: regular14.copyWith(color: dark1),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 24,
+                              ),
+                              SvgPicture.asset(
+                                'assets/icons/left.svg',
+                                // ignore: deprecated_member_use
+                                color: dark1,
+                                height: 24,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // GopayLater
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      width: 24,
+                      height: 24,
+                      decoration: BoxDecoration(
+                        color: blue1,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: SvgPicture.asset(
+                        'assets/icons/gosend.svg',
+                        // ignore: deprecated_member_use
+                        color: Colors.white,
+                        width: 24,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 6,
+                    ),
+                    Flexible(
+                      fit: FlexFit.tight,
+                      child: Text(
+                        'Gopaylater',
+                        style: bold18.copyWith(color: dark1),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                Text(
+                  'Lebih hemat pake GoPayLater',
+                  style: bold16.copyWith(color: dark1),
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                Text(
+                  'Yuk, belnaja di Tokopedia pake GoPay Later dan nikmatin cashback-nya~',
+                  style: semibold14.copyWith(color: dark2),
+                ),
+              ],
+            ),
+          ),
+
+          const SizedBox(
+            height: 24,
+          ),
+
+          // News
+          ...news.map(
+            (item) => Padding(
+              padding: const EdgeInsets.only(
+                bottom: 24,
+                left: 16,
+                right: 16,
+              ),
+              child: Container(
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(color: dark4),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.asset('assets/images/${item.image}'),
+                    Padding(
+                      padding: const EdgeInsets.all(24),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            item.title,
+                            style: bold16.copyWith(color: dark1),
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            item.description,
+                            style: regular14.copyWith(color: dark2),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
